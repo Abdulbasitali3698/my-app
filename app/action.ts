ac1db8c3bf2ffc12f4ae893a4add02788edfc0da
Abdulbasitali3698 <43988292+Abdulbasitali3698@users.noreply.github.com>
2024-04-08 13:42:03 +0500

    .

--- app/action.ts
+++ app/action.ts
@@ -1,7 +1,12 @@
 "use server"
 
  export const getData = async ()=>{
-    const fetchData = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const res = await fetchData.json();
-    return res;
+    try {
+        const fetchData = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const res = await fetchData.json();
+        return res;
+    } catch (error) {
+        console.log("Error in Fetching Data", error);
+    }
+    
 }
